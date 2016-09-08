15c5627d49859ccd40905f40c537b0b642205f24
Pedro Fernandes Filho <pedro.fernandesfilho@gmail.com>
2016-09-07 22:05:26 -0300

    Add Chocolatey profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,14 @@
 ﻿$scriptPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
 # Enable posh-git
+
 if (Test-Path "$scriptPath\Modules\posh-git\profile.example.ps1") {
     . $scriptPath\Modules\posh-git\profile.example.ps1
+}
+
+# Chocolatey profile
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
 }
