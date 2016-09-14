3a61bdaedc398a0c8201ad39346688486e68d3f6
Pedro Fernandes Filho <pedro.fernandesfilho@gmail.com>
2016-09-13 23:02:11 -0300

    Create aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -16,3 +16,32 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
+
+# Set Aliases
+
+# - Git
+
+function add {
+    if ($args) {
+        Invoke-Expression ( "git add " + ($args -join ' ') )
+    } else {
+        git add -A :/
+    }
+}
+Add-Alias st 'git status'
+Add-Alias push 'git push'
+Add-Alias pull 'git pull'
+Add-Alias log 'git log'
+Add-Alias ci 'git commit'
+Add-Alias co 'git checkout'
+Add-Alias dif 'git diff'
+Add-Alias rs 'git reset'
+Add-Alias rb 'git rebase'
+#Add-Alias fixup 'git fixup'
+Add-Alias branch 'git branch'
+Add-Alias tag 'git tag'
+
+# - Unix 
+
+Add-Alias l 'ls'
+Add-Alias ll 'ls -Force'
